10581a7d628f87e8d6437d2dd851b3a7ad5ed671
unknown <hien.ton.gtd@gmail.com>
2021-07-13 21:49:15 +0700

    add the H3

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,14 +1,15 @@
-import React from 'react'; 
-import './Comp.css';
+import React from "react";
+import "./Comp.css";
 
-export default function Logo(){
-    const style = {
-        color:'white',
-        fontSize:'30px'
-    };
-    return(
-        <div>
-            <p style={style}>cibus</p>
-        </div>
-    );
+export default function Logo() {
+  const style = {
+    color: "white",
+    fontSize: "30px",
+  };
+  return (
+    <div>
+      <p style={style}>cibus</p>
+      <h3>Test thêm thẻ H3</h3>
+    </div>
+  );
 }
